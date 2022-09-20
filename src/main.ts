4392b9a53e9ad19eb946f44725e398d94940811f
jaredzieche <jaredzieche@gmail.com>
2022-09-20 08:07:14 -0400

    update build commands

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,89 @@
 import * as core from '@actions/core'
-import {context, getOctokit} from '@actions/github'
+import {context, GitHub} from '@actions/github'
 
-process.on('unhandledRejection', handleError)
-main().catch(handleError)
+async function run(): Promise<void> {
+  try {
+    // Create GitHub client with the API token.
+    const client = new GitHub(core.getInput('token', {required: true}))
 
-async function main(): Promise<void> {
-    const token = core.getInput('token', {required: true});
-    const github = getOctokit(token);
-    const files = await github.rest.pulls.listFiles({
+    // Debug log the payload.
+    core.debug(`Payload keys: ${Object.keys(context.payload)}`)
+
+    // Get event name.
+    const eventName = context.eventName
+
+    // Define the base and head commits to be extracted from the payload.
+    let base: string | undefined
+    let head: string | undefined
+
+    switch (eventName) {
+      case 'pull_request':
+        base = context.payload.pull_request?.base?.sha
+        head = context.payload.pull_request?.head?.sha
+        break
+      case 'push':
+        base = context.payload.before
+        head = context.payload.after
+        break
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+            "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        )
+    }
+
+    // Log the base and head commits
+    core.info(`Base commit: ${base}`)
+    core.info(`Head commit: ${head}`)
+
+    // Ensure that the base and head properties are set on the payload.
+    if (!base || !head) {
+      core.setFailed(
+        `The base and head commits are missing from the payload for this ${context.eventName} event. ` +
+          "Please submit an issue on this action's GitHub repo."
+      )
+
+      // To satisfy TypeScript, even though this is unreachable.
+      base = ''
+      head = ''
+    }
+
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    const response = await client.repos.compareCommits({
+      base,
+      head,
       owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: Number(context.payload.pull_request?.number)
-    });
-    const filenames = files.data.values.name
-    console.log(filenames);
+      repo: context.repo.repo
+    })
+
+    // Ensure that the request was successful.
+    if (response.status !== 200) {
+      core.setFailed(
+        `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
+          "Please submit an issue on this action's GitHub repo."
+      )
+    }
+
+    // Ensure that the head commit is ahead of the base commit.
+    if (response.data.status !== 'ahead') {
+      core.setFailed(
+        `The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
+          "Please submit an issue on this action's GitHub repo."
+      )
+    }
+
+    // Get the changed files from the response payload.
+    const files = response.data.files
+    const all = [] as string[]
+    for (const file of files) {
+      const filename = file.filename
+      all.push(filename)
+    }
+    core.setOutput('all', all)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
-function handleError(err: any): void {
-  console.error(err)
-  core.setFailed(`Unhandled error: ${err}`)
-}
+run()
