94812903c411eea54b870004ff0eb188fa33a9f7
jaredzieche <jaredzieche@gmail.com>
2022-09-20 14:54:40 -0400

    adding inputs and changing to compare basehead

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as core from "@actions/core";
 import { context, getOctokit } from "@actions/github";
+import * as fs from "fs";
+import * as glob from "@actions/glob";
+import * as path from "path"
 
 async function run(): Promise<void> {
   try {
     // Create GitHub client with the API token.
     const token = core.getInput("token", { required: true });
     const octokit = getOctokit(token);
+    const configpath = core.getInput("config_path", { required: true })
+    const fileType = core.getInput("file_type", { required: true })
 
     // Debug log the payload.
     core.debug(`Payload keys: ${Object.keys(context.payload)}`);
@@ -51,12 +56,17 @@ async function run(): Promise<void> {
 
     // Use GitHub's compare two commits API.
     // https://developer.github.com/v3/repos/commits/#compare-two-commits
-    const response = await octokit.rest.repos.compareCommits({
-      base,
-      head,
-      owner: context.repo.owner,
-      repo: context.repo.repo
+    const response = await octokit.rest.repos.compareCommitsWithBasehead({
+      basehead: `${base}...${head}`,
+      owner: 'jaredzieche',
+      repo: 'github-actions-testing'
     });
+    // const response = await octokit.rest.repos.compareCommits({
+    //   base,
+    //   head,
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo
+    // });
 
     // Ensure that the request was successful.
     if (response.status !== 200) {
@@ -75,13 +85,68 @@ async function run(): Promise<void> {
     }
 
     // Get the changed files from the response payload.
-    const files = response.data.files!;
-    const all = [] as string[];
-    for (const file of files) {
-      const filename = file.filename;
-      all.push(filename);
+
+    const commitfiles = response.data.files;
+    const contextdirs = [] as string[];
+    for (const file of commitfiles) {
+      const filename = file.filename
+      if (filename.match(`/${configpath}/`)) {
+        let dirname = path.dirname(filename)
+        contextdirs.push(`${dirname}/${fileType}`);
+      }
     }
-    core.setOutput("all", all);
+    core.setOutput("contextdirs", contextdirs);
+    const newdirs = [];
+    let globPattern = [...new Set(contextdirs)]
+    const globber = await glob.create(globPattern.join('\n'))
+    const globs = await globber.glob()
+    for (const glob of globs) {
+      let newdir = path.dirname(glob)
+      newdirs.push(newdir);
+    }
+    // // console.log(newdirs)
+    // const buildMatrix = {};
+    // const promotionMatrix = {};
+    // buildMatrix.include = []
+    // promotionMatrix.include = []
+    // for (const dir of newdirs) {
+    //   console.log(dir)
+    //   const configFile = `${dir}/config.json`
+    //   // console.log(configFile)
+    //   const config = fs.readFileSync(configFile, 'utf8')
+    //   // console.log(config)
+    //   let obj = JSON.parse(config)
+    //   const mapFile = fs.readFileSync('/Users/jaredzieche/github-actions-testing/src/docker/config.json', 'utf8')
+    //   const registryMap = JSON.parse(mapFile)
+    //   for (const target of obj.targets) {
+    //     var ghEnv = Object.entries(registryMap)
+    //     // console.log(ghEnv)
+    //     for (const [key,value] of Object.entries(registryMap)) {
+    //       for (var t of value) {
+    //         if (t.includes(target)) {
+    //           let gh = key
+    //           // console.log(key)
+    //           buildMatrix.include.push({
+    //             name: dir,
+    //             image: `${obj.image["name"]}:${obj.image["tag"]}`
+    //           })
+    //           promotionMatrix.include.push({
+    //             name: dir,
+    //             env: gh,
+    //             targets: target,
+    //             image: `${obj.image["name"]}:${obj.image["tag"]}`
+    //           })
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log(buildMatrix)
+    // console.log(promotionMatrix)
+    // const buildMatrixYaml = yaml.dump(buildMatrix)
+    // console.log(buildMatrixYaml)
+
+
   } catch (error) {
     core.setFailed(`${(error as any)?.message ?? error}`);
   }
