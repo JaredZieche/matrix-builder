9816ee0f9317ccaad9590cc4c00584be76e7b8a8
jaredzieche <jaredzieche@gmail.com>
2022-09-20 22:28:45 -0400

    resolve object iterator, disable strict null checks

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import * as core from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 import * as fs from "fs";
 import * as glob from "@actions/glob";
-import * as path from "path"
+import * as path from "path";
 
 async function run(): Promise<void> {
   try {
     // Create GitHub client with the API token.
     const token = core.getInput("token", { required: true });
     const octokit = getOctokit(token);
-    const configpath = core.getInput("config_path", { required: true })
-    const filetype = core.getInput("file_type", { required: true })
-    const mapfile = core.getInput("map_file", { required: true })
+    const configpath = core.getInput("config_path", { required: true });
+    const filetype = core.getInput("file_type", { required: true });
+    const mapfile = core.getInput("map_file", { required: true });
 
     // Debug log the payload.
     core.debug(`Payload keys: ${Object.keys(context.payload)}`);
@@ -89,40 +89,44 @@ async function run(): Promise<void> {
     const commitfiles = response.data.files;
     const contextdirs = [] as string[];
     for (const file of commitfiles) {
-      const filename = file.filename
+      const filename = file.filename;
       if (filename.match(`/${configpath}/`)) {
-        let dirname = path.dirname(filename)
+        let dirname = path.dirname(filename);
         contextdirs.push(`${dirname}/${filetype}`);
       }
     }
 
     const newdirs = [] as string[];
-    let globPattern = [...new Set(contextdirs)]
-    const globber = await glob.create(globPattern.join('\n'))
-    const globs = await globber.glob()
+    let globPattern = [...new Set(contextdirs)];
+    const globber = await glob.create(globPattern.join("\n"));
+    const globs = await globber.glob();
     for (const glob of globs) {
-      let newdir = path.dirname(glob)
+      let newdir = path.dirname(glob);
       newdirs.push(newdir);
     }
 
-    let matrix = {};
-    matrix.include = [];
+    type matrices = { name?: string; env?: string; image?: string };
+    type include = matrices[];
+    const matrix: Object = {};
+
     for (const dir of newdirs) {
-      console.log(dir)
-      const configFile = `${dir}/config.json`
-      const config = fs.readFileSync(configFile, 'utf8')
-      let obj = JSON.parse(config)
-      const map = fs.readFileSync(mapfile, 'utf8')
-      const configmap = JSON.parse(map)
+      console.log(dir);
+      const configFile = `${dir}/config.json`;
+      const config = fs.readFileSync(configFile, "utf8");
+      let obj = JSON.parse(config);
+      const configmap = fs.readFileSync(mapfile, "utf8");
+      let mapobj = JSON.parse(configmap);
       for (const target of obj.targets) {
-        for (const [key,value] of Object.entries(configmap)) {
-          for (var t of value) {
-            if (t.includes(target)) {
-              let gh = key
-              matrix.include.push({
+        for (let [key, value] of Object[Symbol.iterator](mapobj)) {
+          for (const val of value) {
+            if (val.includes(target)) {
+              let gh = key;
+              const include: include = [];
+              include.push({
                 name: dir,
+                env: gh,
                 image: `${obj.image["name"]}:${obj.image["tag"]}`
-              })
+              });
             }
           }
         }
@@ -134,8 +138,6 @@ async function run(): Promise<void> {
 
     core.setOutput("matrix", matrix);
     core.setOutput("contextdirs", contextdirs);
-
-
   } catch (error) {
     core.setFailed(`${(error as any)?.message ?? error}`);
   }
